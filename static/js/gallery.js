255af9dc38486eda4ec2995d89deb28a7615d75c
gbhardy <gbhardy@umich.edu>
2017-07-14 17:47:42 -0400

    pivoting to attempt custom slider

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -1,13 +1,44 @@
 //gallery.js
 
 $(document).ready(function() {
-    $("#light-slider").lightSlider({
+    var slideer = $("#light-slider").lightSlider({
         vertical:true,
-        item:1,
-        loop:true,
+        item:3,
+        loop:false,
         enableTouch:true,
         enableDrag:true,
         freeMove:true,
-        controls:true
+        controls:true,
+        adaptiveHeight:false,
+        prevHtml:"<img src=\"/static/images/up-arrow.png\"/>",
+        nextHtml:"<img src=\"/static/images/down-arrow.png\"/>",
+        slideMargin:10
     });
+
+    $(window).resize(function() {
+        //$('#light-slider img').width($('.img-slide-box li').width());
+        var item = 2;
+        var verticalHeight = item * $('.img-slide-box') + item * 10;
+        slider = $("#light-slider").lightSlider({
+            vertical:true,
+            item:item,
+            loop:false,
+            enableTouch:true,
+            enableDrag:true,
+            freeMove:true,
+            controls:true,
+            adaptiveHeight:false,
+            prevHtml:"<img src=\"/static/images/up-arrow.png\"/>",
+            nextHtml:"<img src=\"/static/images/down-arrow.png\"/>",
+            slideMargin:10,
+            verticalHeight:verticalHeight
+        });
+        slider.refresh();
+        $('#light-slider img').height($('.img-slide-box').height());
+        $('.lSPager').hide();
+    });
+
+    //Everything to run on start
+    $('.lSPager').hide();
+    $('#light-slider img').height($('.img-slide-box').width());
 });
