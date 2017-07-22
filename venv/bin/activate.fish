7fc2fc7b512dbd62dd99f666a9687e3b3432d391
gbhardy <gbhardy@umich.edu>
2017-07-22 00:32:57 -0400

    left pymsql and flask in reqs.txt

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Grant/Documents/repositories/JaneHardyStudios/venv"
+set -gx VIRTUAL_ENV "/Users/Grant/repos/JaneHardyStudios/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
